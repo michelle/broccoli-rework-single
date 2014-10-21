c3f8f932724b2bb5b44c2382a958c28f6293340a
Michelle Bu <michelle@stripe.com>
2014-10-21 15:31:14 -0700

    Add semicolons and cleanup

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 'use strict';
 
+var includePathSearcher = require('include-path-searcher');
 var assign = require('object-assign');
 var path = require('path');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 
-var CachingWriter = require('broccoli-caching-writer')
+var CachingWriter = require('broccoli-caching-writer');
 var Rework = require('rework');
 
-function ReworkCompiler(inputTree, inputFile, outputFile, opts) {
+function ReworkCompiler(inputTrees, inputFile, outputFile, opts) {
   if (!(this instanceof ReworkCompiler)) {
-    return new ReworkCompiler(inputTree, inputFile, outputFile, opts)
+    return new ReworkCompiler(inputTrees, inputFile, outputFile, opts);
   }
 
-  CachingWriter.apply(this, [inputTree, opts])
+  CachingWriter.apply(this, [inputTrees, opts]);
 
-  this.inputFile = inputFile
-  this.outputFile = outputFile
-  this.sourceDir = inputTree
-  this.opts = opts || {}
+  this.inputFile = inputFile;
+  this.outputFile = outputFile;
+  this.opts = opts || {};
 }
 
-ReworkCompiler.prototype = Object.create(CachingWriter.prototype)
-ReworkCompiler.prototype.constructor = ReworkCompiler
+ReworkCompiler.prototype = Object.create(CachingWriter.prototype);
+ReworkCompiler.prototype.constructor = ReworkCompiler;
 
 ReworkCompiler.prototype.updateCache = function(srcPaths, destDir) {
-  var destFile = destDir + '/' + this.outputFile
+  var destFile = destDir + '/' + this.outputFile;
+
   mkdirp.sync(path.dirname(destFile));
+
+  var filename = includePathSearcher.findFileSync(this.inputFile, srcPaths);
   var data = fs.readFileSync(filename, 'utf8');
 
   var rework = new Rework(data, {
-    source: this.sourceDir
+    source: filename
   });
 
   if (this.opts.use) {
